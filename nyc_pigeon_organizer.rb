3bc31ef396b564eb489d8bff5a99929354920b84
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-14 18:34:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -31,7 +31,7 @@ def nyc_pigeon_organizer(data)
             final_hash[inner_key] = {}
           end 
           if(!final_hash[inner_key][outer_key])
-            final_hash[inner_key][outer_key] = []
+            final_hash[inner_key][outer_key] = [key]
           end 
           binding.pry
       end 
