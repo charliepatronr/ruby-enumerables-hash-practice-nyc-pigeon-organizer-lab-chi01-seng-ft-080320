16b9ca0c2d0b01d9a30b156e6076c59e64ea14fb
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-14 18:33:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,46 @@
+require 'pry'
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  result = data.each_with_object({}) do |(outer_key, outer_value), final_hash|
+    outer_value.each do |key, value|
+      value.each do |inner_key|
+        if (!final_hash[inner_key])
+            final_hash[inner_key] = {}
+          end 
+          if(!final_hash[inner_key][outer_key])
+            final_hash[inner_key][outer_key] = []
+          end 
+          binding.pry
+      end 
+
+    end 
+
+  end 
+ pp result 
 end
+
+
+nyc_pigeon_organizer(pigeon_data)
